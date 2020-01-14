714ea6b7db6d41926e20c27031f80639dcccff1c
Detonation <detonation240@gmail.com>
2020-01-14 11:52:28 -0500

    114

--- ethanol-sensor-standalone.ino
+++ ethanol-sensor-standalone.ino
@@ -1,19 +1,30 @@
-// Adafruit_SSD1306 - Version: Latest 
-// Fixed Compatibility with 4 wire 0.96 OLED
-// Requires Arduino (nano accepts 12v natively), 0.96 OLED, 4.7k-ohm resistor (x1), GM PN#13577429 or compatible GM Flex sensor.
-// Aaron S. (Detonation)
+/*
+Aaron S. (Detonation) 2020
+Parts: Arduino Nano, 0.96 OLED 4 Pin, 4.7K & 3.3k Ohm resistor 0.1uF Capacitor, GM PN#13577429 or compatible GM Flex sensor
+OLED Display
+GND - GND
+VCC - 5V
+A4 - SDA
+A5 - SLC
 
+Flex Fuel Sensor
+D8 - Flex input (Must use 4.7kohm resistor with 5v divider from Arduino) 
+GND - Vehicle GND
+VCC - Vehicle 12v IGN Switched
 
+Flex Sensor ECU Out
+D11 - To ECU (D11 to One lead of Cap, Other Lead of Cap to Ground) (One lead of 3.3k Resistor to SAME lead of cap as D11, Other lead of resistor is 0-5v out)
+*/
+
+#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
 int inpPin = 8;
-
-//Define global variables
+int outPin = 11;    //define PWM output, possible pins with LCD and 32khz freq. are 3 and 11 (Nano and Uno)
 volatile uint16_t revTick;    //Ticks per revolution
 uint16_t pwm_output  = 0;      //integer for storing PWM value (0-255 value)
 int HZ = 0;                  //unsigned 16bit integer for storing HZ input
@@ -38,31 +49,37 @@ void setupTimer()   // setup timer1
   TCCR1B = 132;    // (10000100) Falling edge trigger, Timer = CPU Clock/256, noise cancellation on
   TCCR1C = 0;      // normal mode
   TIMSK1 = 33;     // (00100001) Input capture and overflow interupts enabled
-  
+
   TCNT1 = 0;       // start from 0
 }
-
 ISR(TIMER1_CAPT_vect)    // PULSE DETECTED!  (interrupt automatically triggered, not called by main program)
 {
   revTick = ICR1;      // save duration of last revolution
   TCNT1 = 0;       // restart timer for next revolution
 }
-
 ISR(TIMER1_OVF_vect)    // counter overflow/timeout
 { revTick = 0; }        // Ticks per second = 0
 
 
 
+
 void setup() {
+  pinMode(inpPin,INPUT);
+  setPwmFrequency(outPin,1); //Modify frequency on PWM output
+  
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  //display.drawPixel(10, 10, WHITE);
+  display.display();
+  delay(1000);
+
   setupTimer();
   Serial.begin(9600);
-
-  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
-  // init done
 }
 
+
 void loop() {
+  
   getfueltemp(inpPin); //read fuel temp from input duty cycle
   
   if (revTick > 0) // Avoid dividing by zero, sample in the HZ
@@ -78,22 +95,38 @@ void loop() {
   
   if (ethanol > 99) // Avoid overflow in PWM
   {ethanol = 99;}
+
+  expectedv = ((((HZ-50.0)*0.01)*4)+0.5);
+/////////////////////////ECU OUT///////////////////////////////////////////////////////
   
-    //Screen calculations
-    display.display();
-    display.clearDisplay();
-    display.setCursor(0,0);
-    display.setTextSize(2);
-    display.setTextColor(WHITE);
+  //calculations
+  pwm_output = 1.1 * (255 * (expectedv/5.0)); //calculate output PWM for ECU
+  //PWM output
+  analogWrite(outPin, pwm_output); //write the PWM value to output pin
+
+/////////////////////////////////////////////////
+///////////////////////////////////////////////// DISPLAY ///////
+/////////////////////////////////////////////////
+///////////////////Title & EGT////////////////////////  
+      display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.print("    DetonationEMS");
+  display.setCursor(0,8);
+  display.setTextSize(2);
+///////////////////E85//////////////////////////
+    //display.setTextSize(1);
     display.print("Eth:  ");
     display.print(ethanol);
-    display.print("%");
-    display.setCursor(0,16);
-    display.print("Temp: ");
+    display.println("%");
+    display.setTextSize(1);
+    display.print("Fuel Temp:   ");
     display.print(fahr);
     display.print("F");
-    
-    delay(250);
+    display.display();
+
+    delay(500);
 }
 
 void getfueltemp(int inpPin){ //read fuel temp from input duty cycle
@@ -119,4 +152,35 @@ void getfueltemp(int inpPin){ //read fuel temp from input duty cycle
   cels = celstemp;
   fahrtemp = ((temperature*1.8)+32);
   fahr = fahrtemp;
+}
+
+void setPwmFrequency(int pin, int divisor) { //This code snippet raises the timers linked to the PWM outputs
+  byte mode;                                 //This way the PWM frequency can be raised or lowered. Prescaler of 1 sets PWM output to 32KHz (pin 3, 11)
+  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 64: mode = 0x03; break;
+      case 256: mode = 0x04; break;
+      case 1024: mode = 0x05; break;
+      default: return;
+    }
+    if(pin == 5 || pin == 6) {
+      TCCR0B = TCCR0B & 0b11111000 | mode;
+    } else {
+      TCCR1B = TCCR1B & 0b11111000 | mode;
+    }
+  } else if(pin == 3 || pin == 11) {
+    switch(divisor) {
+      case 1: mode = 0x01; break;
+      case 8: mode = 0x02; break;
+      case 32: mode = 0x03; break;
+      case 64: mode = 0x04; break;
+      case 128: mode = 0x05; break;
+      case 256: mode = 0x06; break;
+      case 1024: mode = 0x7; break;
+      default: return;
+    }
+    TCCR2B = TCCR2B & 0b11111000 | mode;
+  }
 }
